1c2eec5812338cb68b532340ba5f79dc3526ac80
Athenea Reche Enriquez <atheneareche@gmail.com>
2023-11-21 11:07:52 +0100

    Project complete with some functionalities

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { 
-   SafeAreaView,
-   StyleSheet,
-   Text,
-   View 
-} from 'react-native';
-import { configureStore } from '@reduxjs/toolkit'
-
+import { StyleSheet, Text, View } from 'react-native';
+/* import { configureStore } from '@reduxjs/toolkit'; */
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style= {{fontSize: 40}}>Nombre de mi app</Text>
-      <Text>Bienvenidos a mi app</Text>
-      <StatusBar style="auto" />
-    </View>
-   
+    <NavigationContainer>
+      <TailwindProvider>
+        <View style={styles.container}>
+          <Text className=" ">Recetario</Text>
+          <StatusBar style="auto" />
+        </View>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
 
@@ -28,14 +26,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  containerB: {
-    flex:1,
-    marginTop: StatusBar.currentHeight || 0
-},
-  bodyText: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
 });
